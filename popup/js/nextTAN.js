3769d16485bf39e75e186049198ba7a93edb7213
Arnaud Chesnay <arnaud.chesnay@gmail.com>
2017-12-04 12:17:33 +0100

    Get arrets

--- popup/js/nextTAN.js
+++ popup/js/nextTAN.js
@@ -3,13 +3,21 @@ fetch(req).then(function(res) {
     console.log(res);
 });
 */
-if ("geolocation" in navigator) {
+/*if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition(
         function(position) {
                 console.log(position.coords.latitude, position.coords.longitude);
         }
   );
-  
+
 } else {
   console.log("coucou");
-}
+}*/
+
+$(document).ready(function() {
+    $.getJSON('http://open_preprod.tan.fr/ewp/arrets.json').done(function(data) {
+        $.each(data, function(i, field){
+            $("ul").append('<li>' + field.libelle + '</li>');
+        })
+    });
+});
