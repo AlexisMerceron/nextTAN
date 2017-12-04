e67dffbb18afcb25e82bf37d0829b729c56a13a3
Arnaud Chesnay <arnaud.chesnay@gmail.com>
2017-12-04 14:10:33 +0100

    Autocomplete

--- popup/js/nextTAN.js
+++ popup/js/nextTAN.js
@@ -1,23 +1,13 @@
-/*var req = new Request('http://open_preprod.tan.fr/ewp/arrets.json');
-fetch(req).then(function(res) {
-    console.log(res);
-});
-*/
-/*if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(
-        function(position) {
-                console.log(position.coords.latitude, position.coords.longitude);
-        }
-  );
-
-} else {
-  console.log("coucou");
-}*/
-
 $(document).ready(function() {
+    var arrets = [];
     $.getJSON('http://open_preprod.tan.fr/ewp/arrets.json').done(function(data) {
-        $.each(data, function(i, field){
-            $("select").append('<option>' + field.libelle + '</option>');
+        $.each(data, function(i, field) {
+            arrets.push(field.libelle);
+            console.log(field.libelle)
         })
+    }).then(function() {
+        $("#tags").autocomplete({
+            source: arrets
+        });
     });
 });
