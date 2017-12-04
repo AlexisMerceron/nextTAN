3f377c66e170851ef97aca846bc2ca5b05329370
merceron <alexis.merceron@etu.univ-nantes.fr>
2017-12-04 19:32:38 +0100

    add researchshedule

--- popup/js/nextTAN.js
+++ popup/js/nextTAN.js
@@ -1,31 +1,63 @@
 'use strict';
 
 $(document).ready(function() {
-    function getHoraires(code) {
+
+    // Récupération des prochains départs
+    function getHoraires(code){
+        var depart =[];
+        var d = new Date();
+        $.getJSON('http://open_preprod.tan.fr/ewp/tempsattente.json/' + code).done(function(data) {
+            $.each(data, function(i, depart) {
+                var next = depart.temps;
+                if (!isNaN(parseFloat(depart.temps))){
+                  var min = parseInt(depart.temps.replace(' mn',''));
+                  next = new Date(d.getTime() + (min * 60000));
+                }
+                alert(next);
+            })
+        })
+    };
+
+    // Détect le choix de la ligne et génération de la liste des prochains départs
+    $('#choix_ligne').on('input',function(){
+      var val = document.getElementById("choix_ligne").value
+      var opts = document.getElementById('ligne').childNodes;
+      for (var i = 0; i < opts.length; i++) {
+          if (opts[i].value === val) {
+              var code = opts[i].getAttribute('data-value');
+              getHoraires(code);
+              break;
+          }
+      }
+    });
+
+    // Récupération des ligne passant par l'arret (code)
+    function getLines(code) {
         var arrets = [];
         $.getJSON('http://open_preprod.tan.fr/ewp/tempsattente.json/' + code).done(function(data) {
             $.each(data, function(i, ligne) {
                 if (arrets.indexOf(ligne.arret.codeArret) === -1) {
                     var option = '<option data-value="' + ligne.arret.codeArret + '" value="' + ligne.ligne.numLigne + ' - ' + ligne.terminus + '">';
                     $('#ligne').append(option);
-                    arrets.push(ligne.arret.codeArret);
                 }
             })
         })
     }
 
+    // Détect le choix de l'arret et génération de la liste des lignes passant par cet arret
     $('#choix_arret').on('input', function() {
         var val = document.getElementById("choix_arret").value;
         var opts = document.getElementById('arret').childNodes;
         for (var i = 0; i < opts.length; i++) {
             if (opts[i].value === val) {
                 var code = opts[i].getAttribute('data-value');
-                getHoraires(code);
+                getLines(code);
                 break;
             }
         }
     });
 
+    // Création de la liste des arrêts à moins de 500 m de l'utilisateur
     navigator.geolocation.getCurrentPosition(function(position) {
         var lat = ('' + position.coords.latitude).replace('.', ',');
         var long = ('' + position.coords.longitude).replace('.', ',');
