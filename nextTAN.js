b2a912f02c4364f289c13ac5866dd9b42fc9fad8
Merceron Alexis <alexis.merceron49@gmail.com>
2017-12-04 10:49:23 +0100

    add localisation in js

--- nextTAN.js
+++ nextTAN.js
@@ -3,3 +3,13 @@ fetch(req).then(function(res) {
     console.log(res);
 });
 */
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(
+        function(position) {
+                console.log(position.coords.latitude, position.coords.longitude);
+        }
+  );
+  
+} else {
+  console.log("coucou");
+}
