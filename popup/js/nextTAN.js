8b08f451e2f29628673d915369433db6464cf6f0
Arnaud Chesnay <arnaud.chesnay0gmail.com>
2017-12-04 16:56:11 +0100

    get lignes

--- popup/js/nextTAN.js
+++ popup/js/nextTAN.js
@@ -1,10 +1,15 @@
+'use strict';
+
 $(document).ready(function() {
     function getHoraires(code) {
-        var lignes = [];
+        var arrets = [];
         $.getJSON('http://open_preprod.tan.fr/ewp/tempsattente.json/' + code).done(function(data) {
-            $.each(data, function(i, field) {
-                var option = '<option value="' + field.libelle + '">';
-                
+            $.each(data, function(i, ligne) {
+                if (arrets.indexOf(ligne.arret.codeArret) === -1) {
+                    var option = '<option data-value="' + ligne.arret.codeArret + '" value="' + ligne.ligne.numLigne + ' - ' + ligne.terminus + '">';
+                    $('#ligne').append(option);
+                    arrets.push(ligne.arret.codeArret);
+                }
             })
         })
     }
@@ -15,14 +20,15 @@ $(document).ready(function() {
         for (var i = 0; i < opts.length; i++) {
             if (opts[i].value === val) {
                 var code = opts[i].getAttribute('data-value');
+                getHoraires(code);
                 break;
             }
         }
     });
 
     navigator.geolocation.getCurrentPosition(function(position) {
-        lat = ('' + position.coords.latitude).replace('.', ',');
-        long = ('' + position.coords.longitude).replace('.', ',');
+        var lat = ('' + position.coords.latitude).replace('.', ',');
+        var long = ('' + position.coords.longitude).replace('.', ',');
         $.getJSON('http://open_preprod.tan.fr/ewp/arrets.json/' + lat + '/' + long).done(function(data) {
             $.each(data, function(i, field) {
                 var option = '<option data-value="' + field.codeLieu + '" value="' + field.libelle + '">';
