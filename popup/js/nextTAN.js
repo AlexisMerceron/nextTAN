1dd408674521af53d23c41a77e148ffc8a327592
Arnaud Chesnay <arnaud.chesnay0gmail.com>
2017-12-04 16:18:16 +0100

    on input

--- popup/js/nextTAN.js
+++ popup/js/nextTAN.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
+    function getHoraires(code) {
+        var lignes = [];
+        $.getJSON('http://open_preprod.tan.fr/ewp/tempsattente.json/' + code).done(function(data) {
+            $.each(data, function(i, field) {
+                var option = '<option value="' + field.libelle + '">';
+                
+            })
+        })
+    }
+
+    $('#choix_arret').on('input', function() {
+        var val = document.getElementById("choix_arret").value;
+        var opts = document.getElementById('arret').childNodes;
+        for (var i = 0; i < opts.length; i++) {
+            if (opts[i].value === val) {
+                var code = opts[i].getAttribute('data-value');
+                break;
+            }
+        }
+    });
+
     navigator.geolocation.getCurrentPosition(function(position) {
         lat = ('' + position.coords.latitude).replace('.', ',');
         long = ('' + position.coords.longitude).replace('.', ',');
-        alert('http://open_preprod.tan.fr/ewp/arrets.json/' + lat + '/' + long);
         $.getJSON('http://open_preprod.tan.fr/ewp/arrets.json/' + lat + '/' + long).done(function(data) {
             $.each(data, function(i, field) {
                 var option = '<option data-value="' + field.codeLieu + '" value="' + field.libelle + '">';
